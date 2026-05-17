c16081bd6ca58d40f83e41409a3bf5b12c2bddf1
Sam Ezeh <sam.z.ezeh@gmail.com>
2026-05-17 15:26:17 +0100

    Add nonblocking collectives to header file

--- fortran_headers.h
+++ fortran_headers.h
@@ -8,6 +8,7 @@
 
 // TODO: Can I automatically generate these from mpi.h?
 extern "C" {
+    // Utilities
     DECLARE_PMPI_VARIANTS(init, INIT, (int *ierror))
     DECLARE_PMPI_VARIANTS(init_thread, INIT_THREAD, (int *required, int *provided, int *ierror))
     DECLARE_PMPI_VARIANTS(finalize, FINALIZE, (int *ierror))
@@ -15,6 +16,7 @@ extern "C" {
     DECLARE_PMPI_VARIANTS(initialized, INITIALIZED, (int *flag, int *ierror))
     DECLARE_PMPI_VARIANTS(finalized, FINALIZED, (int *flag, int *ierror))
 
+    // Communicators
     DECLARE_PMPI_VARIANTS(comm_rank, COMM_RANK, (MPI_Fint *comm, int *rank, int *ierror))
     DECLARE_PMPI_VARIANTS(comm_size, COMM_SIZE, (MPI_Fint *comm, int *size, int *ierror))
     DECLARE_PMPI_VARIANTS(comm_dup, COMM_DUP, (MPI_Fint *comm, MPI_Fint *newcomm_f, int *ierror))
@@ -27,6 +29,7 @@ extern "C" {
     DECLARE_PMPI_VARIANTS(comm_get_name, COMM_GET_NAME, (MPI_Fint *comm, char *name, int *len, int *ierror, int name_len))
     DECLARE_PMPI_VARIANTS(comm_set_name, COMM_SET_NAME, (MPI_Fint *comm, char *name, int *ierror, int name_len))
 
+    // Point to point
     DECLARE_PMPI_VARIANTS(send, SEND, (const void *buf, int *count, MPI_Fint *datatype, int *dest, int *tag, MPI_Fint *comm, int *ierror))
     DECLARE_PMPI_VARIANTS(recv, RECV, (void *buf, int *count, MPI_Fint *datatype, int *source, int *tag, MPI_Fint *comm, MPI_Fint *status_f, int *ierror))
     DECLARE_PMPI_VARIANTS(sendrecv, SENDRECV, (const void *sendbuf, int *sendcount, MPI_Fint *sendtype, int *dest, int *sendtag, void *recvbuf, int *recvcount, MPI_Fint *recvtype, int *source, int *recvtag, MPI_Fint *comm, MPI_Fint *status_f, int *ierror))
@@ -34,10 +37,19 @@ extern "C" {
     DECLARE_PMPI_VARIANTS(irecv, IRECV, (void *buf, int *count, MPI_Fint *datatype, int *source, int *tag, MPI_Fint *comm, MPI_Fint *request_f, int *ierror))
     DECLARE_PMPI_VARIANTS(wait, WAIT, (MPI_Fint *request_f, MPI_Fint *status_f, int *ierror))
     DECLARE_PMPI_VARIANTS(waitall, WAITALL, (int *count, MPI_Fint *requests_f, MPI_Fint *statuses_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(waitany, WAITANY, (int *count, MPI_Fint *requests_f, int *index, MPI_Fint *status_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(waitsome, WAITSOME, (int *incount, MPI_Fint *requests_f, int *outcount, int *indices, MPI_Fint *statuses_f, int *ierror))
     DECLARE_PMPI_VARIANTS(probe, PROBE, (int *source, int *tag, MPI_Fint *comm, MPI_Fint *status_f, int *ierror))
     DECLARE_PMPI_VARIANTS(iprobe, IPROBE, (int *source, int *tag, MPI_Fint *comm, int *flag, MPI_Fint *status_f, int *ierror))
     DECLARE_PMPI_VARIANTS(test, TEST, (MPI_Fint *request_f, int *flag, MPI_Fint *status_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(testany, TESTANY, (int *count, MPI_Fint *requests_f, int *index, int *flag, MPI_Fint *status_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(testsome, TESTSOME, (int *incount, MPI_Fint *requests_f, int *outcount, int *indices, MPI_Fint *statuses_f, int *ierror))
 
+    // Persistent communication
+    DECLARE_PMPI_VARIANTS(send_init, SEND_INIT, (const void *buf, int *count, MPI_Fint *datatype, int *dest, int *tag, MPI_Fint *comm, MPI_Fint *request_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(recv_init, RECV_INIT, (void *buf, int *count, MPI_Fint *datatype, int *source, int *tag, MPI_Fint *comm, MPI_Fint *request_f, int *ierror))
+
+    // Blocking collectives
     DECLARE_PMPI_VARIANTS(barrier, BARRIER, (MPI_Fint *comm, int *ierror))
     DECLARE_PMPI_VARIANTS(bcast, BCAST, (void *buffer, int *count, MPI_Fint *datatype, int *root, MPI_Fint *comm, int *ierror))
     DECLARE_PMPI_VARIANTS(reduce, REDUCE, (const void *sendbuf, void *recvbuf, int *count, MPI_Fint *datatype, MPI_Fint *op, int *root, MPI_Fint *comm, int *ierror))
@@ -49,8 +61,19 @@ extern "C" {
     DECLARE_PMPI_VARIANTS(reduce_scatter, REDUCE_SCATTER, (const void *sendbuf, void *recvbuf, int *recvcounts, MPI_Fint *datatype, MPI_Fint *op, MPI_Fint *comm, int *ierror))
     DECLARE_PMPI_VARIANTS(scan, SCAN, (const void *sendbuf, void *recvbuf, int *count, MPI_Fint *datatype, MPI_Fint *op, MPI_Fint *comm, int *ierror))
 
+    // Nonblocking collectives
+    DECLARE_PMPI_VARIANTS(ibarrier, IBARRIER, (MPI_Fint *comm, MPI_Fint *request_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(ibcast, IBCAST, (void *buffer, int *count, MPI_Fint *datatype, int *root, MPI_Fint *comm, MPI_Fint *request_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(ireduce, IREDUCE, (const void *sendbuf, void *recvbuf, int *count, MPI_Fint *datatype, MPI_Fint *op, int *root, MPI_Fint *comm, MPI_Fint *request_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(iallreduce, IALLREDUCE, (const void *sendbuf, void *recvbuf, int *count, MPI_Fint *datatype, MPI_Fint *op, MPI_Fint *comm, MPI_Fint *request_f, int *ierror))
+
+    // Vector collectives
     DECLARE_PMPI_VARIANTS(gatherv, GATHERV, (const void *sendbuf, int *sendcount, MPI_Fint *sendtype, void *recvbuf, int *recvcounts, int *displs, MPI_Fint *recvtype, int *root, MPI_Fint *comm, int *ierror))
     DECLARE_PMPI_VARIANTS(scatterv, SCATTERV, (const void *sendbuf, int *sendcounts, int *displs, MPI_Fint *sendtype, void *recvbuf, int *recvcount, MPI_Fint *recvtype, int *root, MPI_Fint *comm, int *ierror))
     DECLARE_PMPI_VARIANTS(allgatherv, ALLGATHERV, (const void *sendbuf, int *sendcount, MPI_Fint *sendtype, void *recvbuf, int *recvcounts, int *displs, MPI_Fint *recvtype, MPI_Fint *comm, int *ierror))
     DECLARE_PMPI_VARIANTS(alltoallv, ALLTOALLV, (const void *sendbuf, int *sendcounts, int *sdispls, MPI_Fint *sendtype, void *recvbuf, int *recvcounts, int *rdispls, MPI_Fint *recvtype, MPI_Fint *comm, int *ierror))
+
+    DECLARE_PMPI_VARIANTS(win_create, WIN_CREATE, (void *base, MPI_Aint *size, int *disp_unit, MPI_Fint *info, MPI_Fint *comm, MPI_Fint *win_f, int *ierror))
+    DECLARE_PMPI_VARIANTS(file_open, FILE_OPEN, (MPI_Fint *comm, const char *filename, int *amode, MPI_Fint *info, MPI_Fint *file_f, int *ierror, int filename_len))
+
 }
