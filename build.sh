8aa1ec09b632d701753a0776ae5c5c1a6b22f8a1
Sam Ezeh <sam.z.ezeh@gmail.com>
2026-05-17 15:25:48 +0100

    Specify CC and CXX in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+export CC="${CC:-mpicc}"
+export CXX="${CXX:-mpicxx}"
+echo "Building with compiler CC=$CC"
+echo "Building with compiler CXX=$CXX"
+rm -rf build
 mkdir build
 cd build
 cmake ..
